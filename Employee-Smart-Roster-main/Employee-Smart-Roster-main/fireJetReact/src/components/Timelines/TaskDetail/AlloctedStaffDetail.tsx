65465923925944abe0242514878ec8db14547a78
wmlim014 <166711243+wmlim014@users.noreply.github.com>
2025-04-13 16:14:51 +0800

    View Task Detail

--- Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/components/Timelines/TaskDetail/AlloctedStaffDetail.tsx
+++ Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/components/Timelines/TaskDetail/AlloctedStaffDetail.tsx
@@ -1,2 +1,29 @@
 import './MoreInfor.css'
 import '../../../../public/styles/common.css'
+import { MdContactPhone, MdOutlineMailOutline } from '../../../../public/Icons.js'
+
+interface AllocatedStaffDetailProps {
+    allocatedStaff: any;
+}
+
+const AllocatedStaffDetail = ({ allocatedStaff = [] }: AllocatedStaffDetailProps) => {
+    // console.log(allocatedStaffID)
+    
+    return (
+        <div className="allocated-staff-content">
+            <p className="title">{allocatedStaff.jobTitle}</p>
+            {/* Display contact number */}
+            <div className="allocated-staff-contact-no">
+                <MdContactPhone className='App-popup-content-icon'/>
+                {allocatedStaff.hpNo}
+            </div>
+            {/* Display email */}
+            <div className="allocated-staff-email">
+                <MdOutlineMailOutline className='App-popup-content-icon'/>
+                {allocatedStaff.email}
+            </div>
+        </div>
+    )
+}
+
+export default AllocatedStaffDetail
