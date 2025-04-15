26641e3ad9f56e422f0902a9e933f2f0cc56aab5
Yeo Yet Han <yhyeo009@mymail.sim.edu.sg>
2025-04-15 16:31:33 +0800

    Revert "Merge pull request #26 from Dewie123/WM-Dev"
    This reverts commit b2bdf1baf218d447b708db108b62a4838f305744, reversing
    changes made to 137fa90da682af594a11dfe3b5eefdfba6eb6c51.

--- Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/components/Timelines/TaskDetail/MoreDetail.tsx
+++ Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/components/Timelines/TaskDetail/MoreDetail.tsx
@@ -1,83 +1,18 @@
-import { useState, useEffect } from 'react';
-import { useAuth } from '../../../AuthContext.js';
-import { useAlert } from '../../../components/PromptAlert/AlertContext.js';
-
-import CompanyController from '../../../controller/CompanyController.js';
-import TimelineController from '../../../controller/TimelineController';
-
 import './MoreInfor.css'
 import '../../../../public/styles/common.css'
-import { VscDebugBreakpointData, } from '../../../../public/Icons.js'
 
 interface MoreDetailProps {
-    roleID: number;
-    skillID: number;
+    task?: any;
 }
 
-const { getCompanyRoles, getCompanySkillsets } = CompanyController
-const { getRoleNeededForTask, getSkillNeededForTask } = TimelineController
-
-const MoreDetail = ({ roleID, skillID }: MoreDetailProps) => {
-    if (!roleID) return;
-    if (!skillID) return;
-    // console.log(roleID, skillID)
-    const { user } = useAuth();
-    // console.log(user)
-    const { showAlert } = useAlert();
-    const [ roleNeeded, setRoleNeeded ] = useState<any>([]);
-    const [ skillNeeded, setSkillNeeded ] = useState<any>([]);
-
-    const fetchRoleNSkillNeeded = async () => {
-        try {
-            // Fetch all roles attached to this company
-            let allRoles = await getCompanyRoles(user?.UID);
-            allRoles = allRoles.roleName;
-            // Get role needed
-            const role = getRoleNeededForTask(allRoles, roleID);
-            // console.log(role)
-            setRoleNeeded(role)
-
-            // Fetch all skillsets attached to this company
-            let allSkills = await getCompanySkillsets(user?.UID);
-            allSkills = allSkills.skillSets;
-            // Get skill needed
-            const skill = getSkillNeededForTask(allSkills, skillID);
-            console.log(skill)
-            setSkillNeeded(skill)
-        } catch (error) {
-            showAlert(
-                "fetchRoleNSkillNeeded",
-                "Fetch data error",
-                error instanceof Error ? error.message : String(error),
-                { type: 'error' }
-            )
-        }
-    };
-    useEffect(() => {fetchRoleNSkillNeeded()}, [roleID, skillID])
-    // useEffect(() => {console.log(roleNeeded[0], skillNeeded[0])}, [roleID, skillID])
+const MoreDetail = ({ task=[] }: MoreDetailProps) => {
 
     return (
-        <div className='role-and-skill-needed-content'>
-            {roleNeeded?.length === 1 && (
-            <div className="role-needed">
-                <p className="title">Role Needed</p>
-                <div className="role-needed-data">
-                    <VscDebugBreakpointData className='App-popup-content-icon'/>
-                    <p className='main-data'>{roleNeeded[0].roleName}</p>
-                </div>
-            </div>
-            )}
-            {skillNeeded?.length === 1 && (
-            <div className="skill-needed">
-                <p className="title">Skill Needed</p>
-                <div className="skill-needed-data">
-                    <VscDebugBreakpointData className='App-popup-content-icon'/>
-                    <p className='main-data'>{skillNeeded[0].skillSetName}</p>
-                </div>
-            </div>
-            )}
+        <div>
+
         </div>
     )
+
 }
 
 export default MoreDetail;
--- Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/controller/CompanyController.js
+++ Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/controller/CompanyController.js
@@ -61,55 +61,6 @@ async function getCompany (uid){
     }
 }
 
-async function getCompanyRoles (boID){
-    // console.log(boID)
-    const body = {
-        business_owner_id: boID,
-    }
-    try{
-        const response = await fetch('https://e27fn45lod.execute-api.ap-southeast-2.amazonaws.com/dev/business-owner/company/role/view', {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        if(!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message || `HTTP error status: ${response.status}`);
-        }
-        const data = await response.json();
-        // console.log(data);
-
-        return await data;
-    } catch (error) {
-        console.error(`Network error for UID ${uid}: \n`, error);
-        throw new Error(`Failed to fetch company data: ${error.message}`);
-    }
-}
-
-async function getCompanySkillsets (boID){
-    const body = {
-        business_owner_id: boID,
-    }
-    try{
-        const response = await fetch('https://e27fn45lod.execute-api.ap-southeast-2.amazonaws.com/dev/business-owner/company/skillset/view', {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        if(!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message || `HTTP error status: ${response.status}`);
-        }
-        const data = await response.json();
-        // console.log(data);
-
-        return await data;
-    } catch (error) {
-        console.error(`Network error for UID ${uid}: \n`, error);
-        throw new Error(`Failed to fetch company data: ${error.message}`);
-    }
-}
-
 function GetCompanyRoles (){
     const data = [
         {
@@ -182,10 +133,8 @@ function handleFilterUENBizName(companies, filterString){
 export default {
     getCompanies,
     getCompany,
-    getCompanyRoles,
-    getCompanySkillsets,
-    GetCompanyRoles, // Not Using
-    GetCompanySkillsets, // Not Using
+    GetCompanyRoles,
+    GetCompanySkillsets,
     handleSelectedDetail,
     handleFilterUENBizName,
 }
