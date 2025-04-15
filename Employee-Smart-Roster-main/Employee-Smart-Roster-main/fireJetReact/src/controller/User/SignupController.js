491da76bd85dd09c61192a8b2cf9d315f071ad45
Yeo Yet Han <yhyeo009@mymail.sim.edu.sg>
2025-04-15 16:30:54 +0800

    Revert "Registration"
    This reverts commit 0e32c13e56b3dc8064da69d58aee20fe566456d3.

--- Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/controller/User/SignupController.js
+++ Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/controller/User/SignupController.js
@@ -8,7 +8,35 @@ function validateEmail (email){
         return ""
 }
 
+async function createRegisRequest (){
+    const body = {
+        registrationID: registrationID,
+        status: status,
+        reasonOfReject:reasonOfReject,
+    };
+
+    try{
+        const response = await fetch('https://e27fn45lod.execute-api.ap-southeast-2.amazonaws.com/dev/s3/guest/register', {
+            method: 'PATCH',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if(!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || `HTTP error status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+
+        return await data;
+    } catch(error) {
+        console.error(`Network error for UID ${uid}: \n`, error);
+        throw new Error(`Failed to fetch company data: ${error.message}`);
+    }
+}
+
 
 export default {
     validateEmail,
+    createRegisRequest,
 }
